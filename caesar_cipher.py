00e98b97c77629b941a4eae07c4941970c023e19
savelyevcs <49760594+savelyevcs@users.noreply.github.com>
2019-06-27 03:34:40 -0700

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,50 +3,73 @@ class CaesarCipher():
         This class is a special realize of caesar cipher
 
         THE SPECIAL METHODS:
-        __init__(text, shift) type: main -> class initialization
-        __repr__() type: return -> encrypted or decrypted result
+        __init__(text = None, shift = None) -> class initialization
+        __repr__() -> returns encrypted or decrypted result
 
         THE PERSONAL METHODS:
-        encrypt() type: process -> text encrypting
-        decrypt() type: process -> text decrypting
-        get_result() type: return -> encrypted or decrypted result
-        get_shift() type: return -> shift
+        encrypt() -> text encrypting
+        decrypt() -> text decrypting
+        get_result() -> returns encrypted or decrypted value
+        get_encrypted_result() -> returns encrypted value
+        get_decrypted_result() -> returns decrypted value
     '''
     _ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
     _ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    def __init__(self, text, shift):
+    def __init__(self, text = None, shift = None):
+        if not any((text, shift)):
+            raise Exception('You did not enter one of the values')
+            return
+
         self.text = list(text)
         self.shift = shift
-        self._buffer = []
-
-    def __repr__(self):
-        return ''.join(self.text)
+        self.encrypt_buffer = ''
+        self.decrypt_buffer = ''
 
     def encrypt(self):
         for i in self.text:
             if not i.isalpha(): 
-                self._buffer.append(i)
+                self.encrypt_buffer += i
                 continue
             elif i in list(CaesarCipher._ascii_uppercase):
-                shifted_index = (
+                next_index = (
                     CaesarCipher._ascii_uppercase.index(i) + self.shift
-                ) % len(CaesarCipher._ascii_uppercase)
+                ) % 26
 
-                self._buffer.append(
-                    CaesarCipher._ascii_uppercase[shifted_index]
-                )
+                self.encrypt_buffer += CaesarCipher._ascii_uppercase[next_index]
             elif i in list(CaesarCipher._ascii_lowercase):
-                shifted_index = (
+                next_index = (
                     CaesarCipher._ascii_lowercase.index(i) + self.shift
-                ) % len(CaesarCipher._ascii_lowercase)
+                ) % 26
 
-                self._buffer.append(
-                    CaesarCipher._ascii_lowercase[shifted_index]
-                )
+                self.encrypt_buffer += CaesarCipher._ascii_lowercase[next_index]
+    
+    def decrypt(self):
+        for i in self.encrypt_buffer:
+            if not i.isalpha():
+                self.decrypt_buffer += i
+                continue
+            elif i in list(CaesarCipher._ascii_uppercase):
+                prev_index = (
+                    CaesarCipher._ascii_uppercase.index(i) - self.shift
+                ) % 26
 
+                self.decrypt_buffer += CaesarCipher._ascii_uppercase[prev_index]
+            elif i in list(CaesarCipher._ascii_lowercase):
+                prev_index = (
+                    CaesarCipher._ascii_lowercase.index(i) - self.shift
+                ) % 26
+
+                self.decrypt_buffer += CaesarCipher._ascii_lowercase[prev_index]
+
+    def get_encrypted_result(self):
+        return self.encrypt_buffer
+
+    def get_decrypted_result(self):
+        return self.decrypt_buffer
+    
     def get_result(self):
-        return ''.join(self._buffer)
-
-    def get_shift(self):
-        return self.shift
+        return {
+            'encrypted': self.encrypt_buffer,
+            'decrypted': self.decrypt_buffer
+        }
